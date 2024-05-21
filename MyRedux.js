822bd742796fd68a48240d88396d0c6e2e4753b4
CoolShivas <shivas2710cool00@gmail.com>
2024-05-21 16:06:56 +0530

    Formation of Reducer

--- MyRedux.js
+++ MyRedux.js
@@ -1,6 +1,12 @@
 // importing redux from the library;
 const redux = require('redux');
 
+// Formation of reducer;
+const reducer = (store, action) => {
+    return store;
+};
+
+
 // Creating store for reducer;
-const store = redux.createStore();
+const store = redux.createStore(reducer);
 
