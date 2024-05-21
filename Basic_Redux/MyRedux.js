71c86cc8632a833bb2e87a88cb6e097475ebcf1b
CoolShivas <shivas2710cool00@gmail.com>
2024-05-21 16:43:16 +0530

    Getting Initial value 2 instead of 1

--- Basic_Redux/MyRedux.js
+++ Basic_Redux/MyRedux.js
@@ -1,4 +1,4 @@
-// Now, we have imported the MyRedux.js file inside or near the .gitignore file because of node MyRedux.js is not working properly if we move more inside the src then also it will not work;
+// Now, we have moved the MyRedux.js file inside or near the .gitignore file because of node MyRedux.js is not working properly if we move more inside the src then also it will not work;
 // importing redux from the library;
 const redux = require('redux');
 
@@ -9,7 +9,7 @@ const INITIAL_VALUE = {
 
 // Formation of reducer;
 const reducer = (store = INITIAL_VALUE, action) => {
-    return {counter : store.counter + 1};
+    return {counter : store.counter + 1}; // Passing initial value to store i.e, 1;
 };
 
 
@@ -19,7 +19,7 @@ const store = redux.createStore(reducer);
 // Creating subscriber for store to call state;
 const subscriber = () => {
     const state = store.getState();
-    console.log(state);
+    console.log(state);// Here, we are getting the output as 2 instead of 1; 
 };
 
 
